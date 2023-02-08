da293698a2b15efeaffb7ade2e50ffbc8e9bf61e
abranllis <47560487+abranllis@users.noreply.github.com>
2023-02-08 12:11:41 +0100

    1

--- src/DownloadFile.java
+++ src/DownloadFile.java
@@ -1,27 +1,39 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 public class DownloadFile {
-
     public static void main(String[] args) throws Exception {
-        String url = "https://www.example.com/textfile.txt";
+        String fileUrl = "https://www.forta.es/wp-prue-123.txt";
+        String fileName = "src/wp-prue-123.txt";
 
-        CompletableFuture<String> fileContentFuture = CompletableFuture.supplyAsync(() -> {
+        CompletableFuture<Void> downloadFile = CompletableFuture.runAsync(() -> {
             try {
-                URL fileURL = new URL(url);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(fileURL.openStream()));
-                return reader.lines().collect(Collectors.joining("\n"));
+                URL website = new URL(fileUrl);
+                ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                FileOutputStream fos = new FileOutputStream(fileName);
+                fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                fos.close();
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         });
 
-        fileContentFuture.thenAccept(fileContent -> {
-            System.out.println("Contenido del archivo:");
-            System.out.println(fileContent);
+        CompletableFuture<Void> openFile = downloadFile.thenRun(() -> {
+            try {
+                String[] command = { "notepad.exe", fileName };
+                ProcessBuilder builder = new ProcessBuilder(command);
+                builder.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
+
+        openFile.get();
     }
 }
